36b380a8738f290fc0201f53d2cff9ecd87ed609
nonex97 <ngrguric97@gmail.com>
2026-06-05 22:30:24 +0200

    App that has a list of employees with their ID number. It asks user to enter an ID number and gives the name of the employee. It checks for a valid input and valid ID number.

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -1,4 +1,36 @@
-﻿
+﻿// Create a Dictionary list of employee IDs and the name that goes with the ID.
+// Fill in a few records then ask the user for their ID and return their name.
 
 
+using System.Globalization;
 
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[1] = "Neven";
+employees[2] = "John";
+employees[3] = "Tim";
+employees[4] = "Yukica";
+employees[5] = "Sue";
+
+string idText;
+int id;
+bool isValid = false;
+
+
+do
+{
+    Console.Write("Please enter employee ID: ");
+    idText = Console.ReadLine();
+    isValid = int.TryParse(idText, out id);
+    if (isValid == false)
+    {
+        Console.WriteLine("Invalid input, please try again.");
+    }
+    else if (id < 0 || id > employees.Count - 1)
+    {
+        Console.WriteLine($"Employee with ID number {id} does not exist, please try again.");
+        isValid = false;
+    }
+} while (isValid == false);
+
+Console.WriteLine($"The employee with ID number {id} is: {employees[id]}");
